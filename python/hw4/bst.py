5e2d93839ea959e40be1cec20ed39839a451c317
mesheshe <alias49@live.com>
2021-05-19 09:56:49 -0700

    hw

--- python/hw4/bst.py
+++ python/hw4/bst.py
@@ -1,7 +1,10 @@
 # Course: CS261 - Data Structures
 # Student Name: Elias Meshesha
-# Assignment:
-# Description:
+# Assignment: 4
+# Description: The following code implements a binary search tree which has by creating
+#              a bst class which has the following methods: add, contains, remove, get_first,
+#              remove_first, pre-order-traversal, in-order-traversal, post-order-traversal, 
+#              and etc.
 
 
 class Stack:
@@ -125,7 +128,7 @@ class BST:
 
     def add(self, value: object) -> None:
         """
-        TODO: Write this implementation
+        Adds the given value to the bst 
         """
         newNode = TreeNode(value)
         if self.root is None:
@@ -145,6 +148,10 @@ class BST:
                 parentNode.left = newNode
             
     def findNode(self, node, value, parentNode = None):
+        """
+        helper function that takes in a value and a node and traverse
+        the tree starting from that node and if a node containing the value
+        is found, the node as well as its parent is returned"""
         if node is None or node.value == value:
             return node, parentNode
         if value > node.value:
@@ -154,7 +161,7 @@ class BST:
 
     def contains(self, value: object) -> bool:
         """
-        TODO: Write this implementation
+        returns true if a node containing the value passed in is found
         """
         if self.root is None:
             return False
@@ -163,7 +170,7 @@ class BST:
 
     def get_first(self) -> object:
         """
-        TODO: Write this implementation
+        gets the value of the root node if it exists otherwise returns None
         """
         if self.root is None:
             return None
@@ -171,7 +178,7 @@ class BST:
     
     def remove_first(self) -> bool:
         """
-        TODO: Write this implementation 
+        Removes the root node and returns True if done so otherwise returns False
         """
         if self.root is None:
             return False  
@@ -193,6 +200,14 @@ class BST:
         return True
     
     def replaceSuccessor(self, node, parentNode):
+        """
+        helper function for the remove function, this helper function assumes 
+        that we have gotten through all the preconditions and that we pass it in 
+        the node we want to remove. This function will find the successor of this node
+        and make that takes it place as well as give the right children of the successor
+        to th successor's previous parent's left child Returns true once the replacement 
+        is done 
+        """
         parentSucc = None
         Succ = node.right
         while Succ.left is not None:
@@ -212,7 +227,8 @@ class BST:
 
     def remove(self, value) -> bool:
         """
-        TODO: Write this implementation
+        Removes the first node found that contains the value given and returns True 
+        after the operation has completed, otherwise returns False if not found. 
         """
         node, parentNode = self.findNode(self.root, value)
         if node is None:
@@ -239,6 +255,11 @@ class BST:
         return True
                 
     def pre_order(self, q, currNode):
+        """
+        helper function that creates the queue by traversing the bst. Algo goes
+        like this: if we see a node for the first time, we add it to the queue, then
+        traverse left by recursively calling this function again and then we go right. 
+        """
         if currNode is not None:
             q.enqueue(currNode.value)
             self.pre_order(q, currNode.left)
@@ -247,13 +268,21 @@ class BST:
 
     def pre_order_traversal(self) -> Queue:
         """
-        TODO: Write this implementation
+        Returns a queue that contains values gotten by traversing the bst in a
+        pre-order-traversal manner. 
         """
         if self.root is None:
             return  Queue()
         return self.pre_order(Queue(), self.root)
     
     def in_order(self, q, currNode):
+        """
+        helper function that creates the queue by traversing the bst. Algo goes
+        like this: We go all the way to the leftmost node, then we add the value
+        to the queue, then we check if that value has a right node, if it does 
+        it explores that path, otherwise it backs out a level to repeat the process 
+        of writing down the value and then checking if a right node exists
+        """
         if currNode is not None:
             self.in_order(q, currNode.left)
             q.enqueue(currNode.value)
@@ -262,13 +291,19 @@ class BST:
 
     def in_order_traversal(self) -> Queue:
         """
-        TODO: Write this implementation
+        Returns a queue that contains values gotten by traversing the bst in a
+        in-order-traversal manner. So Queue is sorted.
         """
         if self.root is None:
             return Queue()
         return self.in_order(Queue(), self.root)
 
     def post_order(self, q, currNode):
+        """
+        helper function that creates the queue by traversing the bst. Algo goes
+        like this: Algo similar to in_order, except now we are also making sure that 
+        when we write to queue, the value is for a node that has no right children
+        """
         if currNode is not None:
             self.post_order(q, currNode.left)
             self.post_order(q, currNode.right)
@@ -277,13 +312,23 @@ class BST:
 
     def post_order_traversal(self) -> Queue:
         """
-        TODO: Write this implementation
+        Returns a queue that contains values gotten by traversing the bst in a
+        post-order-traversal manner. 
         """
         if self.root is None:
             return Queue()
         return self.post_order(Queue(), self.root)
 
     def by_level(self, q, q_throwaway = None):
+        """
+        Helper function that returns a queue that is traversed by level
+        Algo goes like this: It utilizes two queues. One throwawy, the other 
+        a return queue. This function initializes the throwaway by enqueueing self.root.
+        And while it is q_throwaway is not empty, it does the following:
+        add the value of the node at the front of q_throwaway, to the return 
+        queue. Then enqueue the left and right child of the dequeued node 
+        respectively and if they exist. Then repeat. 
+        """
         if q_throwaway is None:
             q_throwaway = Queue()
             q_throwaway.enqueue(self.root)
@@ -299,18 +344,26 @@ class BST:
 
     def by_level_traversal(self) -> Queue:
         """
-        TODO: Write this implementation
+        Returns a queue that traverses the tree by level
         """
         if self.root is None:
             return Queue()
         return self.by_level(Queue())
 
-    def is_full_rec(self, node, que): 
+    def is_full_rec(self, node, que):
+        """
+        by level traversal. It first checks to make sure all internal nodes have
+        two children, by checking if the current node only has one child, if that 
+        passes, then the left child and the right child are added to the queue, after 
+        making sure that the node is not a leaf node. The function is called recursively
+        and for the node argument, we get it by dequeuing the queue. It returns true 
+        if the queue is empty.  
+        """ 
         if node is not None:
             if (node.left is None and node.right is not None) or (node.left is not None and node.right is None):
                 return False
-            if node.left is not None:
-                que.enqueue(node.left)
+            if node.left is not None: # notice that we only need to check one, since node.right is also not none
+                que.enqueue(node.left) # if this is true 
                 que.enqueue(node.right)
         if que.is_empty():
             return True
@@ -318,13 +371,23 @@ class BST:
 
     def is_full(self) -> bool:
         """
-        TODO: Write this implementation
+        Returns true if the bst is full otherwise returns false
+        All internal nodes have two children
         """
         if self.root is None:
             return True 
         return self.is_full_rec(self.root, Queue())
 
     def is_complete_rec(self, node, height = 0, maxHeight = None, tof = True):
+        """
+        Helper function that determines whether a bst is complete, pre-order-traversal.
+        Returns a tuple called a height which we care about and a bool which we don't 
+        and is used internally. The height will return -1, if it is not complete, otherwise
+        returns some number. This function, in a nutshell, looks at the height of the leaves,
+        it makes sure that they are either all the same and if not, it makes sure that the 
+        leaves going from left to right, only have a height change of 1 and that height 
+        change only happens once, with the leftmost leaf's height > rightmost leaf's height.   
+        """
         # This stop condition makes sure that the leaves are all at the same level 
         # or only have a difference of 1
         if node is None: # ignore the additional 1 added due to none since it will cancel out 
@@ -349,18 +412,21 @@ class BST:
 
     def is_complete(self) -> bool:
         """
-        TODO: Write this implementation
+        Returns true if the bst is complete otherwise returns false
+        Except for the last level, every level before it is full, and the 
+        for the last level, either it is full or if it is not, the node is
+        added to the left child of the parent a level higher, before being
+        added to the right.  
         """
         if self.root is None:
             return True 
         maxHeight, tof = self.is_complete_rec(self.root)
-        if maxHeight == -1:
-            return False
-        return True 
+        return maxHeight != -1
 
     def is_perfect(self) -> bool:
         """
-        TODO: Write this implementation
+        Returns true if the bst is perfect otherwise returns false.
+        The BST is full and all leaves have the same height 
         """
         if self.root is None:
             return True 
@@ -369,6 +435,12 @@ class BST:
         return size == 2**(height + 1) - 1
 
     def s(self, node, q = None, q2 = None, count = 1):
+        """
+        Helper function that traverse the bst by level. It has a queue ADT which it 
+        uses to store the left and rigt child of the current node (if they exist). 
+        On each recurisve call, the queue is dqueued and the count is incremented by 1.
+        When the queue is empty, it retuns the count.   
+        """
         if q2 is None:
             q2 = Queue()
         if node is not None:
@@ -382,13 +454,18 @@ class BST:
 
     def size(self) -> int:
         """
-        TODO: Write this implementation
+        Returns the number of nodes in the binary search tree
         """
         if self.root is None:
             return 0
         return self.s(self.root)
 
     def h(self,node, height = 0, maxVal = 0):
+        """
+        Helper function that traverses the bst in a pre-order-traversal that
+        checks first if the current node is a leaf and if so it increments
+        counter and returns it. 
+        """
         if node is None:
             height -= 1  # it counted the case where node is None, gotta uncount that
             if height > maxVal:
@@ -401,15 +478,21 @@ class BST:
 
     def height(self) -> int:
         """
-        TODO: Write this implementation
+        Returns the maximum depth of a binary tree, if it is empty it retuns
+        -1 
         """
         if self.root is None:
             return -1
         return self.h(self.root)
 
     def leaves(self, node, counter = 0):
+        """
+        Helper function that traverses the bst in a pre-order-traversal that
+        checks first if the current node is a leaf and if so it increments counter
+        and returns it. 
+        """
         if node is not None and node.left is None and node.right is None:
-            return counter + 1
+            return counter + 1     # + 1 because the current node needs to be added
         if node is not None:
             counter = self.leaves(node.left, counter)
             counter = self.leaves(node.right, counter)
@@ -417,19 +500,25 @@ class BST:
 
     def count_leaves(self) -> int:
         """
-        TODO: Write this implementation
+        Returns the count of leaves
         """
         if self.root is None:
             return 0
         return self.leaves(self.root)
 
     def unique(self, node, prev = None, curr = None, counter = 0):
+        """
+        Helper function that does an in-order-traversal, and utilizes 
+        twp stacks, one named prev, and the other curr. prev gets curr.top()
+        value pushed to it while curr gets the current node's value pushed to it.
+        Then a comparison is done between these two stack's top(), and if they 
+        are not the same, then the counter is incrememnted. Once this recursion 
+        is done, it returns the counter.
+        """
         if prev is None:
             prev = Stack()
             curr = Stack()
             curr.push(None)
-        if node is None:
-            return counter
         if node is not None:
             counter = self.unique(node.left, prev, curr, counter)
             prev.push(curr.top())
@@ -441,7 +530,7 @@ class BST:
 
     def count_unique(self) -> int:
         """
-        TODO: Write this implementation
+        Counts the number of unique values in the binary search tree.
         """
         if self.root is None:
             return 0
